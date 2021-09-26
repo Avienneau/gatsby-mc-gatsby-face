95a9c67dbff7d4d71921da6e6c1994869719449b
Aaron Vienneau <Avienneau@hometap.com>
2021-09-26 15:42:02 -0400

    Finished Gatsby Tutorial 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby McGatsbyFace",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
